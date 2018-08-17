711ca366dff1c20f6727146075ff15ad77e6f593
laramartin <lmartin@babbel.com>
2018-08-17 13:08:40 +0200

    Top-Level function

---
+++ src/main/kotlin/TopLevelFunction.kt
@@ -0,0 +1,10 @@
+/**
+ * ------------------
+ * Top-Level Function
+ * ------------------
+ *
+ * 1. Decompile to Java
+ * 2. The Java class generated is called "TopLevelFunctionKt"
+ * 3. "TopLevelFunctionKt" Java class declares final static foo() function
+ */
+fun foo() = "bar"

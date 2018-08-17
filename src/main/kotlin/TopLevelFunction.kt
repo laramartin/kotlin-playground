d57ccd6d6c6cf9ef097cfc09cc4520d88828b1ab
laramartin <lmartin@babbel.com>
2018-08-17 13:12:01 +0200

    Add annotation JvmName on Top-level function

--- src/main/kotlin/TopLevelFunction.kt
+++ src/main/kotlin/TopLevelFunction.kt
@@ -6,5 +6,8 @@
  * 1. Decompile to Java
  * 2. The Java class generated is called "TopLevelFunctionKt"
  * 3. "TopLevelFunctionKt" Java class declares final static foo() function
+ * 4. Annotate the file with @file:JvmName("") allows to rename the Java file generated
  */
+
+@file:JvmName("TopLevelFunction")
 fun foo() = "bar"
